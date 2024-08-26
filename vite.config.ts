4e6863f021228a502e2baee95f96e36677d2a51b
Aprendizvp2 <106331402+Aprendizvp2@users.noreply.github.com>
2024-08-26 16:24:35 -0500

    refactor start

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import obfuscator from "rollup-plugin-obfuscator";
+import { minify } from "terser";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "obfuscate",
+      minify: minify,
+      transform(code, id) {
+        if (!id.endsWith(".js")) return;
+        const obfuscatedCode = obfuscator.obfuscate(code, {
+          compact: true,
+          unicodeEscapeSequence: false,
+        });
+        return {
+          code: obfuscatedCode,
+          map: null,
+        };
+      },
+    },
+  ],
+  base: "https://dgtaliza.com/",
+});
