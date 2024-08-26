2a5897e685987c879e823a93a43d83b763989269
Stiven Urresti <106106455+StivenUrresti@users.noreply.github.com>
2024-08-26 17:14:22 -0500

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import obfuscator from "rollup-plugin-obfuscator";
-import { minify } from "terser";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "obfuscate",
-      minify: minify,
-      transform(code, id) {
-        if (!id.endsWith(".js")) return;
-        const obfuscatedCode = obfuscator.obfuscate(code, {
-          compact: true,
-          unicodeEscapeSequence: false,
-        });
-        return {
-          code: obfuscatedCode,
-          map: null,
-        };
-      },
-    },
-  ],
-  base: "https://dgtaliza.com/",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
 });
